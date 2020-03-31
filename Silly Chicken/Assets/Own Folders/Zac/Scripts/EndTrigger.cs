b2d80a33f9fb9fabbc2ed7637469c52c154e7600
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2020-03-31 22:58:09 +1100

    Win Conditions and Controller

---
+++ Silly Chicken/Assets/Own Folders/Zac/Scripts/EndTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("You Win");
+
+            //Loads Win Screen
+            //SceneManager.LoadScene(3);
+        }
+    }
+
+}
---
+++ Silly Chicken/Assets/Own Folders/Zac/Scripts/PlayerHealth.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    //public HealthBar healthBar;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        //healthBar.SetMaxHealth(startingHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthBar.SetHealth(currentHealth);
+
+
+        //Restarts entire game if player dies
+        /*if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene(3);
+        }*/
+
+        if(currentHealth <= 0)
+        {
+            RestartScene();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+        {
+            //RestartScene()
+
+            currentHealth = 0;
+        }
+
+        if(other.gameObject.CompareTag("Farmer"))
+        {
+            //Hurt()
+
+            //currentHealth -= 20;
+        }
+
+        if(other.gameObject.CompareTag("Fox"))
+        {
+            //Hurt()
+
+            //currentHealth -= 20;
+        }
+    }
+
+    public void RestartScene()
+    {
+        //Insert death animation??
+
+        Scene thisScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(thisScene.name);
+    }
+
+    public void Hurt()
+    {
+        //Insert hurt animation??
+    }
+}
