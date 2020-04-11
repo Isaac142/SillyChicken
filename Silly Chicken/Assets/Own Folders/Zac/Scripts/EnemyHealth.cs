92bb49ba40e99f9568d6393f12cce229801b7504
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2020-04-11 17:42:35 +1000

    Enemy Spawn and Enemy Health
    Gave enemy health so player could kill it and so it would respawn if it died.

---
+++ Silly Chicken/Assets/Own Folders/Zac/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    public Spawn spawn;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        spawn = FindObjectOfType<Spawn>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth <=1)
+        {
+            Spawn.instance.callSpawn();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Silly Chicken/Assets/Own Folders/Zac/Scripts/Spawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public static Spawn instance;
+    public GameObject enemy;
+    public GameObject spawnPoint;
+
+    private void Awake()
+    {
+        StartCoroutine(WaitToSpawn());
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            WaitToSpawn();
+        }
+    }
+
+    public void callSpawn()
+    {
+        StartCoroutine(WaitToSpawn());
+    }
+
+    public void Spawning()
+    {
+        Instantiate(enemy, spawnPoint.transform.position, Quaternion.identity);
+    }
+
+    public IEnumerator WaitToSpawn()
+    {
+        Debug.Log("Spawn");
+        yield return new WaitForSeconds(4f);
+        Debug.Log("Waited");
+        Spawning();
+    }
+}
